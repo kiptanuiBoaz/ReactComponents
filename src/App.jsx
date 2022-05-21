b38154ae18f6e2d16a3a3cfead6af50c98ee6b10
Kiptanui Boaz <84452739+kiptanuiBoaz@users.noreply.github.com>
2022-05-21 18:22:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from ".components/Heading";
+import List from ".components/list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
